821b048bac0431e3b3249202aa81cb3ebc2fb142
Hector <hposedev@gmail.com>
2022-03-23 19:25:57 +0100

    updated GameViewModel.kt, randomize words and shuffle letter repaired

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,24 +3,56 @@ package com.example.android.unscramble.ui.game
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
-class GameViewModel : ViewModel(){
-    init {
-        Log.d("GameFragment", "GameViewModel created!")
-    }
-    private var score = 0
-    private var currentWordCount = 0
-    private var _count = 0
+class GameViewModel : ViewModel() {
 
 
-    val count: Int
-        get() = _count
+    private var _score = 0
+    val score: Int
+        get() = _score
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+    /*
+* Returns true if the current word count is less than MAX_NO_OF_WORDS.
+* Updates the next word.
+*/
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
     }
 }
